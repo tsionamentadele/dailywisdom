dce573900ef6538fe727cc854371c86d284d750e
zion <tsionamentadele@gmail.com>
2025-10-22 03:58:35 +0300

    add try/catch to preven crash

--- fetchdata.py
+++ fetchdata.py
@@ -2,11 +2,12 @@ import requests
 
 print("Welcome to your daily bite of wisdom!")
 
-response= requests.get("https://zenquotes.io/api/random")
-if response.status_code == 200:
-    data=response.json()
-    quote=data[0]['q']
-    author=data[0]['a']
+try:
+    response = requests.get("https://zenquotes.io/api/random")
+    response.raise_for_status()
+    data = response.json()
+    quote = data[0]['q']
+    author = data[0]['a']
     print(f'"{quote}" - {author}')
-else:
+except requests.exceptions.RequestException as e:
     print("Failed to retrieve quote. Please try again later.")
